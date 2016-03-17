e7143eb6a27134c811e46dfb41f2dd65f64efd9e
q02031990 <quy02031990@gmail.com>
2016-03-17 18:35:04 +0100

    hien thi duoc tabhost nhung chua chay duoc Fragment

--- app/src/main/java/tiengduc123/com/tabhosttest/MainActivity.java
+++ app/src/main/java/tiengduc123/com/tabhosttest/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements TabHost.OnTabChan
 
     ViewPager viewPager;
     TabHost tabHost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
